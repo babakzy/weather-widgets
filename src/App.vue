b791e915d1908709c1ec254c1e8f2cd7af1fb3bc
Babak <babaksdz@gmail.com>
2023-01-17 15:26:15 +0330

    widget-container and sections html

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,49 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import wdiget1 from './components/wdiget1.vue'
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  fetch("https://api.openweathermap.org/data/2.5/weather?lat=38.0797&lon=46.3002&appid=97c3dcd58e47ab9b00baac7d422371d3")
+    .then(x => x.text())
+    .then(data => console.log(data))
+
+})
+
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
   </header>
 
+
+
+
+
   <main>
-    <TheWelcome />
+
+    <wdiget1 />
+
   </main>
 </template>
 
 <style scoped>
+body {
+  display: block;
+}
+
 header {
   line-height: 1.5;
+  display: block;
+  width: 100%;
+}
+
+main {
+  margin: 70px;
 }
 
 .logo {
   display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+  margin: auto;
 }
 </style>
